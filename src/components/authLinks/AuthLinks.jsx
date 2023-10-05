37394e535dc5a55f983dcb505340dae762a188ce
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-10-05 19:18:09 +0300

    done
    menu bg fix need

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,12 +1,20 @@
+"use client"
+
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
+import { useContext } from 'react';
+import { ThemeContext } from "@/context/ThemeContext";
 
 const AuthLinks = () => {
+  const [open, setOpen] = useState(false)
+  const {theme} = useContext(ThemeContext);
 
   //temporary
   const status = "notauthenticated"
   return (
-    <>
+    <div
+    className='flex gap-3 xl:gap-4 items-center'
+    >
       {status === "notauthenticated" ? (
       <Link href="/login">Login</Link>
     ) : (
@@ -15,7 +23,35 @@ const AuthLinks = () => {
         <span>Logout</span>
       </>
     )}
-    </>
+    {/*burger*/}
+    <div
+    onClick={() => setOpen(!open)}
+    className='w-[20px] h-[20px] xs:flex lgl:hidden flex-col justify-between cursor-pointer '
+    >
+      <div className={`w-full h-[3px] ${theme === "dark" ? 'bg-[#ddd]' : 'bg-black'}`}></div>
+      <div className={`w-full h-[3px] ${theme === "dark" ? 'bg-[#ddd]' : 'bg-black'}`}></div>
+      <div className={`w-full h-[3px] ${theme === "dark" ? 'bg-[#ddd]' : 'bg-black'}`}></div>
+    </div>
+    {open && (
+      <div
+      className={`absolute left-0 top-[100px] h-screen w-full 
+      ${theme === "dark" ? 'bg-[#ddd]' : 'bg-black'}
+      flex flex-col items-center justify-center gap-10 text-4xl`}
+      >
+        <Link href="/">Homepage</Link>
+        <Link href="/">About</Link>
+        <Link href="/">Contact</Link>
+        {status === "notauthenticated" ? (
+          <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/write">Write</Link>
+              <span>Logout</span>
+            </>
+        )}
+      </div>
+    )}
+    </div>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     className='flex items-center justify-between h-[100px]' 
     >
         <div
-        className='flex gap-2 flex-1'
+        className='xs:hidden lgl:flex gap-2 flex-1'
         >
             <Image src="/facebook.png" alt="facebook" width={24} height={24}/>
             <Image src="/instagram.png" alt="instagram" width={24} height={24}/>
@@ -18,17 +18,17 @@ const Navbar = () => {
             <Image src="/youtube.png" alt="youtube" width={24} height={24}/>
         </div>
         <div
-        className='text-4xl font-bold flex-1 text-center'
+        className='xs:text-2xl lgl:text-4xl font-bold flex-1 xs:text-left lgl:text-center '
         >
           farmerblog
         </div>
         <ul
-        className='flex gap-4 flex-1 text-xl items-center'
+        className='flex gap-3 xl:gap-4 flex-1 xs:text-lg lgl:text-xl items-center xs:justify-end'
         >
           <li><Link href="/"/><ThemeToggle/></li>
-          <li><Link href="/"/>Homepage</li>
-          <li><Link href="/"/>Contact</li>
-          <li><Link href="/"/>About</li>
+          <li className='xs:hidden lgl:flex'><Link href="/"/>Homepage</li>
+          <li className='xs:hidden lgl:flex'><Link href="/"/>Contact</li>
+          <li className='xs:hidden lgl:flex'><Link href="/"/>About</li>
           <li><Link href="/"/><AuthLinks/></li>
         </ul>
     </div>
