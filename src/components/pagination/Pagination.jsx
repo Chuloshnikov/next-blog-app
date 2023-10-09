9ad41340df00cc860112c60b75ceb3722f75a00f
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-10-09 18:25:37 +0300

    done

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 
 const Pagination = () => {
   return (
-    <div>Pagination</div>
+    <div
+    className='flex justify-between'
+    >
+      <button
+      className='w-[100px] border-none p-[16px] bg-red-500 text-white cursor-pointer'
+      >
+        Previous
+      </button>
+      <button
+      className='w-[100px] border-none p-[16px] bg-red-500 text-white cursor-pointer'
+      >
+        Next
+      </button>
+    </div>
   )
 }
 
