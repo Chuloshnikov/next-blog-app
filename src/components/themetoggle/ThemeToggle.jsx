4c10bc0df209f3c1ce2118649eb632403fc300b9
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-10-23 16:34:24 +0300

    done

--- src/components/themetoggle/ThemeToggle.jsx
+++ src/components/themetoggle/ThemeToggle.jsx
@@ -1,26 +1,24 @@
 "use client"
 
-import React from 'react';
+import React, { useContext } from 'react';
 import Image from 'next/image';
-import { UseContext } from 'react';
 import { ThemeContext } from "@/context/ThemeContext";
 
-const themeToggle = () => {
-
-  const {toggle, theme} = UseContext(ThemeContext);
+const ThemeToggle = () => {
+  const { toggle, theme } = useContext(ThemeContext);
 
   return (
     <div
-    className={`w-[42px] h-[22px] rounded-3xl ${theme === "light" ? 'bg-black' : "bg-white"} flex items-center justify-between relative`}
+      className={`w-[42px] h-[22px] rounded-3xl ${theme === "light" ? 'bg-black' : "bg-white"} flex items-center justify-between relative`}
     >
-      <Image src="/moon.png" alt="togglemoon" width={14} height={14}/>
+      <Image src="/moon.png" alt="togglemoon" width={14} height={14} />
       <div
-      onClick={toggle}
-      className={`w-[15px] h-[15px] rounded-full absolute ${theme === "dark" ? 'left-[1px] bg-[#0f172a] cursor-pointer' : 'right-[1px] bg-white cursor-pointer'}`}
+        onClick={toggle}
+        className={`w-[15px] h-[15px] rounded-full absolute ${theme === "dark" ? 'left-[1px] bg-[#0f172a] cursor-pointer' : 'right-[1px] bg-white cursor-pointer'}`}
       ></div>
-      <Image src="/sun.png" alt="togglesun" width={14} height={14}/>
+      <Image src="/sun.png" alt="togglesun" width={14} height={14} />
     </div>
-  )
+  );
 }
 
-export default themeToggle;
+export default ThemeToggle;
